c7fdc6b0965afec26c545cb7e269fde27f6ea7b7
Joaquin Mendoza <joaquinmendoza707@gmail.com>
2024-11-26 15:46:30 -0800

    Increase filtering on cloc script

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -3,7 +3,8 @@
 # Count the lines of code in the ice-track directory (excluding certain directories)
 
 # exclude the following directories
-excludes="__pycache__,migrations,venv,instance,docs"
+excludes="__pycache__,migrations,venv,instance,docs,"
+include_exts="py,html,js,css,sh"
 
 # count the lines of code
-cloc . --exclude-dir=$excludes
+cloc . --exclude-dir=$excludes --include-ext=$include_exts
